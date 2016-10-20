2d95750ad1887e1ac19cb38a099467a84ec43d3e
Leslie Xin <leslie.xin@hotmail.com>
2016-10-19 23:53:26 -0400

    Create Third_2ViewController.swift

---
+++ Third_2ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  Third_2ViewController.swift
+//  Masseyhacks_App3
+//
+//  Created by Mehta on 2016-05-21.
+//  Copyright © 2016 iOSAppWorkshop. All rights reserved.
+//
+
+import UIKit
+
+class Third_2ViewController: UIViewController {
+
+    @IBOutlet weak var Name: UITextField!
+    
+    @IBOutlet weak var Email: UITextField!
+    
+    @IBOutlet weak var Organization: UITextField!
+    
+    @IBOutlet weak var Phone: UITextField!
+    
+    @IBOutlet weak var Training: UITextField!
+    
+    @IBOutlet weak var Date: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
