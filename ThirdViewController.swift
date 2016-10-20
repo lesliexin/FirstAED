2b6c6a1ba0cde9eb14cdb3f5089ee5f8e44bbb19
Leslie Xin <leslie.xin@hotmail.com>
2016-10-19 23:52:25 -0400

    Create ThirdViewController.swift

---
+++ ThirdViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ThirdViewController.swift
+//  Masseyhacks_App3
+//
+//  Created by Mehta on 2016-05-21.
+//  Copyright © 2016 iOSAppWorkshop. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+   
+    @IBOutlet weak var Login: UIButton!
+
+    @IBOutlet weak var Reg: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
